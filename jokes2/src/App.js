2e15fd632161fa3a6a51912b9802d42d43f33c1f
juliainkaxo <89917375+juliainkaxo@users.noreply.github.com>
2021-09-27 11:55:09 +0300

    geolocation

--- jokes2/src/App.js
+++ jokes2/src/App.js
@@ -1,42 +1,40 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
-const URL = 'https://api.jokes.one/jod'
+const URL = 'https://api.jokes.one/jod';
 
 function App() {
   const [title, setTitle] = useState('');
   const [text, setText] = useState('');
 
-  
+  async function getJoke(e) {
+    e.preventDefault()
+    try {
+      const response = await fetch(URL);
 
-  useEffect(() => {
-    async function getJoke(e) {
-      try {
-        const response = await fetch(URL);
-
-        if (response.ok) {
-          const json = await response.json();
-          const joke = json.contents.jokes[0].joke;
-          setTitle(joke.title);
-          setText(joke.text);
-        } else {
-          alert('Error retrieving joke!');
-        }
-      } catch (err) {
-        alert(err);
+      if (response.ok) {
+        const json = await response.json();
+        const joke = json.contents.jokes[0].joke;
+        setTitle(joke.title);
+        setText(joke.text);
+      } else {
+        alert('Error retrieving joke!');
       }
+    } catch (err) {
+      alert(err);
     }
-    return (
-      <div style={{ margin: 50 }}>
-        <h1>Joke of the day</h1>
-        <form onSubmit={getJoke}>
-          <h3>{title}</h3>
-          <p>{text}</p>
-        </form>
-      </div>
-    );
-  });
+  }
 
+  return (
+    <div style={{ margin: 50 }}>
+      <h1>Joke of the day</h1>
+      <form onSubmit={getJoke}>
+        <button>Vitsi</button>
+        <h3>{title}</h3>
+        <p>{text}</p>
+      </form>
+    </div>
+  );
 }
 
 export default App;
